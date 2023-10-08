336397262094b6e7856d918667f8946a539c8493
Александр Загородний <git.hub.zagar@gmail.com>
2023-10-08 17:10:07 +0200

    implemented registration

---
+++ src/main/java/com/example/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.security;
+
+import com.example.exception.EntityNotFoundException;
+import com.example.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(
+                () -> new EntityNotFoundException("Can't find user by email: " + username)
+        );
+    }
+}
